da0c14accfba6f9c62b59e2781cd0b3c3b7b3f78
oneboss <123@example.com>
2023-02-05 11:44:38 +0300

    dom_zad_01 and gitignore

--- dom_zad_01/Program.cs
+++ dom_zad_01/Program.cs
@@ -1,6 +1,15 @@
 ﻿// на вход принимает два числа и выдает, какое число большее, а какое меньшее
 
-Console.Write("Введите первое число");
-int = int.Parse
-Console.Write("ВВедите второе число");
-int = int.Parse
+Console.Clear();
+Console.Write("Введите первое число: ");
+int One = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int Two = int.Parse(Console.ReadLine());
+if(One > Two)
+{
+    Console.Write(One + " больше " + Two);
+}
+else
+{
+    Console.Write(Two + " больше " + One);
+}
