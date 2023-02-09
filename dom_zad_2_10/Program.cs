b008a0bfc9eda1da69ffb81cdcea9007c14e0627
oneboss <123@example.com>
2023-02-09 10:50:10 +0300

    dom_zad_2_10

---
+++ dom_zad_2_10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на 
+// выходе показывает вторую цифру этого числа
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+int a1 = num / 100;
+int a2 = num %10;
+int result = ((num - a1*100) - a2)/10;
+Console.WriteLine($"{num} -> {result}");
+
