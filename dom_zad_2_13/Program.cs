8306e1b82c97f8d586baa5b3f7bf68565a4dedf3
oneboss <123@example.com>
2023-02-09 10:56:47 +0300

    dom_zad_2_13

---
+++ dom_zad_2_13/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ))");
+int number = int.Parse(Console.ReadLine());
+Console.Write(number%10);
+
