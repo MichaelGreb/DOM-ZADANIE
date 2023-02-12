34b441b3c41d4ff3924f08ac32ec4feaad846cda
oneboss <123@example.com>
2023-02-12 19:08:07 +0300

    dom_zad_3_23

---
+++ dom_zad_3_23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+while (count<=N)
+{
+    Console.WriteLine($"{count*count*count} ,");
+    count++;
+}
+Console.WriteLine($"{count*count*count} .");
+
+
