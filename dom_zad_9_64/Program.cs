fe926c582cdce31139c7c7a84848d2c9f2886c86
oneboss <123@example.com>
2023-02-24 20:58:48 +0300

    add dom_zad_9_64

---
+++ dom_zad_9_64/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Clear();
+
+Console.WriteLine("Задайте число N: ");
+int userN = int.Parse(Console.ReadLine()!)!;
+NaturalNumbers(userN);
+void NaturalNumbers (int n)
+{
+    Console.WriteLine($" {n}");
+    if (n == 1)
+    {
+        return;
+    } 
+    else
+    {
+        NaturalNumbers(n-1);
+    }
+}
